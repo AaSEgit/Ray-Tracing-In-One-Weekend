88b6efc031bfda59d857e5ccd015bbfba3365104
AaSEgit <145211978+AaSEgit@users.noreply.github.com>
2024-05-26 16:18:08 -0400

    Limit max depth on ray bounces

--- camera.h
+++ camera.h
@@ -13,6 +13,7 @@ class camera {
         double  aspect_ratio        = 1.0;  // Ratio of image width over height
         int     image_width         = 100;  // Rendered image width in pixel count
         int     samples_per_pixel   = 10;   // Count of random samples for each pixel
+        int     max_depth           = 10;   // Maximum number of ray bounces into scene
         
         // Render an image
         void render(const hittable& world) {
@@ -29,7 +30,7 @@ class camera {
                     for (int sample = 0; sample < samples_per_pixel; sample++) {
                         ray r = get_ray(i, j);
                         // define pixel color
-                        pixel_color += ray_color(r, world);
+                        pixel_color += ray_color(r, max_depth, world);
                     }
                     // write out color components
                     write_color(std::cout, pixel_samples_scale * pixel_color);
@@ -40,12 +41,18 @@ class camera {
         }
 
         // return color for a given scene ray
-        color ray_color(const ray& r, const hittable& world) {
+        color ray_color(const ray& r, int depth, const hittable& world) {
+            // If we've exceeded the ray bounce limit, no more light is gathered
+            // return black outside depth limit
+            if (depth <= 0) {
+                return color(0,0,0);
+            }
+
             hit_record rec;
             if (world.hit(r,interval(0, infinity), rec)) {
                 vec3 direction = random_on_hemisphere(rec.normal);
                 // return matte gray
-                return 0.5 * ray_color(ray(rec.p, direction), world);
+                return 0.5 * ray_color(ray(rec.p, direction), depth-1, world);
             }
 
             vec3 unit_direction = unit_vector(r.direction());
