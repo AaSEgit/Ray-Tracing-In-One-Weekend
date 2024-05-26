c23c4898d49ed6e44cc4819e15c2fa748be1cfb1
AaSEgit <145211978+AaSEgit@users.noreply.github.com>
2024-05-26 16:05:31 -0400

    Matte gray diffuse sphere

--- vec3.h
+++ vec3.h
@@ -47,12 +47,24 @@ class vec3 {
             return *this *= 1/t;
         }
 
+        // returns the length of a vector
         double length() const {
             return sqrt(length_squared());
         }
 
+        // returns the squared length of a vector
         double length_squared() const {
             return e[0]*e[0] + e[1]*e[1] + e[2]*e[2];
+        }
+
+        // default constructor for generating random vector
+        static vec3 random() {
+            return vec3(random_double(), random_double(), random_double());
+        }
+
+        // generates random vector with a defined min and max component values
+        static vec3 random(double min, double max) {
+            return vec3(random_double(min, max), random_double(min, max), random_double(min, max));
         }
 };
 
@@ -110,9 +122,35 @@ inline vec3 cross(const vec3& u, const vec3& v) {
                 u.e[0] * v.e[1] - u.e[2] * v.e[0]);
 }
 
-// unit vector
+// // creates a normal unit vector perpendicular to the surface of a unit sphere at that point
 inline vec3 unit_vector(const vec3& v) {
     return v / v.length();
+}
+
+// generates a random unit vector using a rejection method
+// 1. Generate a random vector inside of the unit sphere (center to surface)
+// 2. Normalize this vector (clamp its value to the surface)
+// 3. Invert the normalized vector if it falls onto the wrong hemisphere
+inline vec3 random_in_unit_sphere() {
+    while (true) {
+        auto p = vec3::random(-1 , 1);
+        if (p.length_squared() < 1)
+            return p;
+    }
+}
+
+// normalizes a random vector to a unit vector on the surface of a unit sphere
+inline vec3 random_unit_vector() {
+    return unit_vector(random_in_unit_sphere());
+}
+
+// inverts a normalized vector if it falls onto the wrong hemisphere
+inline vec3 random_on_hemisphere(const vec3& normal) {
+    vec3 on_unit_sphere = random_unit_vector();
+    if (dot(on_unit_sphere, normal) > 0.0)  // in the same hemipshere as normal
+        return on_unit_sphere;
+    else
+        return -on_unit_sphere;
 }
 
 #endif  // end of header file
