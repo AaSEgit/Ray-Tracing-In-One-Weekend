dec279f21f5fab27a6b103ec3c4bfcb5190af45a
AaSEgit <145211978+AaSEgit@users.noreply.github.com>
2024-07-25 15:29:53 -0400

    Distant view image render

--- main.cc
+++ main.cc
@@ -32,12 +32,12 @@ int main() {
     camera cam;
 
     cam.aspect_ratio        = 16.0 / 9.0;
-    cam.image_width         = 100; //400;
+    cam.image_width         = 400;
     cam.samples_per_pixel   = 100;
     cam.max_depth           = 50;
 
-    cam.vfov = 90;  // orthographic wide-angle view
-    cam.lookfrom = point3(0,2,1);
+    cam.vfov = 90;  // wide-angle view
+    cam.lookfrom = point3(-2,2,1);
     cam.lookat = point3(0,0,-1);
     cam.vup = vec3(0,1,0);
 
