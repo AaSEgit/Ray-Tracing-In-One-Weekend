c2f421809de32127122c5003caf9b232b542828a
AaSEgit <145211978+AaSEgit@users.noreply.github.com>
2024-08-10 11:17:35 -0400

    Camera issue fixed
    - issue was caused by miscalculation of vector cross product in vec3.h cross() function

--- vec3.h
+++ vec3.h
@@ -127,8 +127,8 @@ inline double dot(const vec3& u, const vec3& v) {
 // cross product
 inline vec3 cross(const vec3& u, const vec3& v) {
     return vec3(u.e[1] * v.e[2] - u.e[2] * v.e[1],
-                u.e[2] * v.e[0] - u.e[2] * v.e[2],
-                u.e[0] * v.e[1] - u.e[2] * v.e[0]);
+                u.e[2] * v.e[0] - u.e[0] * v.e[2],
+                u.e[0] * v.e[1] - u.e[1] * v.e[0]);
 }
 
 // // creates a normal unit vector perpendicular to the surface of a unit sphere at that point
